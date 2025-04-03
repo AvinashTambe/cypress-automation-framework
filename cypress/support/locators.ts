0c8f4eeb68c96033ad9596ad128dafff8dee47bc
Avinash Tambe <avinashtambe@Avinashs-MacBook-Air.local>
2025-04-03 22:40:44 +0530

    updated

---
+++ cypress/support/locators.ts
@@ -0,0 +1,12 @@
+export enum HomePageLocators {
+    Logo = "//img[@title='Flipkart']",  // XPath for Flipkart logo
+    SearchBox = "input[placeholder='Search for Products, Brands and More']",  // Search box
+    SearchButton = "button[title='Search for Products, Brands and More']",  // Search button
+    LoginLink = "a[title='Login'] span",  // Login link
+    CartLink = "img[alt='Cart']",  // Cart icon
+    ResellerLink = "(//img[@alt='Become a Seller'])[1]",  // Become a Seller link (XPath)
+    MoreLink = "img[alt='Dropdown with more help links']",  // More dropdown
+    CategoriesLink = "._1ch8e_",  // Categories section
+    Banner = "._8S67Ib"  // Banner element
+  }
+  
--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -1,50 +1,41 @@
+import { HomePageLocators } from "@support/locators";
+
 class HomePage {
-  
-  getLogo(){
-    return cy.xpath("//img[@title='Flipkart']"); //Get the logo element
-  }
+    
+   getLogo = function () {
+        return cy.xpath(HomePageLocators.Logo); //Get the logo element
+    };
+    
+    getSearchBox = function () {
+        return cy.get(HomePageLocators.SearchBox); //Get the search box element
+    };
+    
+    getSearchButton = function () {
+        return cy.get(HomePageLocators.SearchButton); //Get the search button element
+    };
+    
+    getLoginLink = function () {
+        return cy.get(HomePageLocators.LoginLink); //Get the login link element
+    };
+    
+    getCartLink = function () {
+        return cy.get(HomePageLocators.CartLink); //Get the cart link element
+    };
+    
+    getResellerLink = function () {
+        return cy.xpath(HomePageLocators.ResellerLink); //Get the reseller link element
+    };
+    getMoreLink = function () {
+        return cy.get(HomePageLocators.MoreLink); //Get the more link element
+    };
+    
+    getCategoriesLink = function () {
+        return cy.get(HomePageLocators.CategoriesLink); //Get the categories link element
+    };
+   
+    getBanner = function () {
+        return cy.get(HomePageLocators.Banner); //Get the banner element
+    };
+};
 
-  getSearchBox(){
-    return cy.get("input[placeholder='Search for Products, Brands and More']"); //Get the search box element
-  }
-
-  getSearchButton(){
-    return cy.get("button[title='Search for Products, Brands and More']"); //Get the search button element
-  }
-
-  getLoginLink(){
-    return cy.get("a[title='Login'] span"); //Get the login link element
-  }
-
-  getCartLink(){
-    return cy.get("img[alt='Cart']"); //Get the cart link element
-  }
-
-  getResellerLink(){
-    return cy.xpath("(//img[@alt='Become a Seller'])[1]"); //Get the reseller link element
-  }
-
-  getMoreLink(){
-    return cy.get("img[alt='Dropdown with more help links']"); //Get the more link element
-  }
-
-  getCategoriesLink(){
-    return cy.get("._1ch8e_"); //Get the categories link element
-  }
-
-  getBanner(){
-    return cy.get("._8S67Ib"); //Get the banner element
-  }
-
-  /*moveBannerNext(){
-    return cy.get("button[title=Next Slide"); //Move to the next banner element
-  }
-
-  moveBannerPrevious(){
-    return cy.get("button[title=Previous Slide"); //Move to the previous banner element
-  }*/
-
-
-}
-
-export default new HomePage();
+exports.default = new HomePage();
